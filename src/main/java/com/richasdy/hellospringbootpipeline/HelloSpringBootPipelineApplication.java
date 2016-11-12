cb85bb3679b0e40dae2345bb96d0d33b945311de
Donni Richasdy <donni.richasdy@gmail.com>
2016-11-12 11:32:40 +0700

    adding spring

---
+++ src/main/java/com/richasdy/hellospringbootpipeline/HelloSpringBootPipelineApplication.java
@@ -0,0 +1,12 @@
+package com.richasdy.hellospringbootpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootPipelineApplication.class, args);
+	}
+}
